2faea6cd5294526effed228d5f48f61f7c06b0db
PARMESHWAR <parmeshwarpanwar777@gmail.com>
2021-05-15 03:45:19 +0530

    Almost Backend completed

--- api/models/category.js
+++ api/models/category.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
-const categories =  mongoose.Schema({
+const categorySchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name:{type: String, required: true},
-    slug:{type: String, unique: true},
-    parent:{type: String},
-    createdAt:Date,
-    createdBy : {type:mongoose.Schema.Types.ObjectId,ref:'Admin'},
-    updateAt : Date,
-    updateBy : {type:mongoose.Schema.Types.Object, ref:'Admin'} 
+    name: { type: String, required: true },
+    slug: { type: String, unique: true },
+    parent: { type: String },
+    createdAt: Date,
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin' },
+    updatedAt: Date,
+    updatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin' }
 });
 
-module.exports = mongoose.model('Category',categorySchema);
+module.exports = mongoose.model('Category', categorySchema);
